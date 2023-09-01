57ce930b8716f8f9418dd6a7e6db23183fcd1d64
Jordan Harband <ljharb@gmail.com>
2023-08-31 21:19:37 -0700

    [Deps] update `define-properties`, `es-abstract`

--- implementation.js
+++ implementation.js
@@ -1,9 +1,9 @@
 'use strict';
 
-var Get = require('es-abstract/2022/Get');
-var LengthOfArrayLike = require('es-abstract/2022/LengthOfArrayLike');
-var ToObject = require('es-abstract/2022/ToObject');
-var ToString = require('es-abstract/2022/ToString');
+var Get = require('es-abstract/2023/Get');
+var LengthOfArrayLike = require('es-abstract/2023/LengthOfArrayLike');
+var ToObject = require('es-abstract/2023/ToObject');
+var ToString = require('es-abstract/2023/ToString');
 
 var callBound = require('call-bind/callBound');
 
